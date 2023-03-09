249eb3a45f02283d8751e625d042f531ac0247ce
Axel <axelcroizepro@gmail.com>
2023-03-09 21:50:07 +0100

    Added second item to index content

--- nextjs-e-website/src/components/content.js
+++ nextjs-e-website/src/components/content.js
@@ -1,4 +1,4 @@
-import styles from '@/styles/mainshoe.module.css'
+import styles from '@/styles/content.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import React, {useState,useEffect} from 'react'
@@ -6,7 +6,7 @@ import ColorThief from '../../node_modules/colorthief/dist/color-thief.mjs'
 
 function Content({items})
 {
-    const shoe = items[0][1];
+    
     function setBackground()
     {
         const section = document.getElementById('mainshoe');
@@ -18,28 +18,55 @@ function Content({items})
     useEffect(() => {
         setBackground()
     }, [])
-    
-    const files = JSON.parse(shoe.filename);
-    const mainPic = "/images/"+files[0];
-    const url = "/product?id="+ encodeURIComponent(shoe.name) ;
+    const assets = [];
+    const urls = [];
+    const shoes = [];
+    items.forEach(item => {
+        const shoe = item[1];
+        shoes.push(shoe)
+        const files = JSON.parse(shoe.filename);
+        const mainPic = "/images/"+files[0];
+        const url = "/product?id="+ encodeURIComponent(shoe.name) ;
+        assets.push(mainPic)
+        urls.push(url)
+    })
     return(
-        <section id = "mainshoe" className={styles.wrapper}>
-            <div className={styles.text}>
-                <span className={styles.new}>new product</span>
-                <h1>{shoe.name}</h1>
-                <p>{shoe.description}</p>
-                <Link href = {url}  className= {styles.link}>see product</Link>
-            </div>
-            <div>
-                <Image
-                    src= {mainPic}
-                    alt="mainPic"
-                    width={500}
-                    height={500}
-                    priority
-                />
-            </div>
-        </section>
+        <div className={styles.content}>
+            <section id = "mainshoe" className={[styles.mainshoe, 'picwrapper'].join(' ')}>
+                <div className={styles.text}>
+                    <span className={styles.new}>new product</span>
+                    <h1>{shoes[0].name}</h1>
+                    <p>{shoes[0].description}</p>
+                    <Link href = {urls[0]}  className= {styles.link}>see product</Link>
+                </div>
+                <div>
+                    <Image
+                        src= {assets[0]}
+                        alt="mainPic"
+                        width={500}
+                        height={500}
+                        priority
+                    />
+                </div>
+            </section>
+            <section id = "second" className={[styles.second, 'picwrapper'].join(' ')}>
+                <div>
+                    <Image
+                        src= {assets[1]}
+                        alt="mainPic"
+                        width={500}
+                        height={500}
+                        priority
+                    />
+                </div>
+                <div className={styles.text}>
+                    <span className={styles.new}>new product</span>
+                    <h1>{shoes[1].name}</h1>
+                    <p>{shoes[1].description}</p>
+                    <Link href = {urls[1]}  className= {styles.link}>see product</Link>
+                </div>
+            </section>
+        </div>
     )
 }
 
