adf6b8d31a3f255ce52c31a638f011b191eb00bb
Axel <axelcroizepro@gmail.com>
2023-03-14 22:08:42 +0100

    Finished footer and starting mobile display dev

--- nextjs-e-website/src/components/footer.js
+++ nextjs-e-website/src/components/footer.js
@@ -1,18 +1,19 @@
 import styles from "@/styles/footer.module.css"
+import Link from 'next/link'
 
 export default function Footer()
 {
     return(
     <footer className={styles.footer}>
-        <div>
+        <div className={styles.footerLeft}>
             <h7>sneakers</h7>
             <p>Sneakers in an all in one stop to fullfill your style needs. We're a small team of shoes lovers and streetwear specialists who are devoted to helping you shine amongst the crowd.Come and visit our demo facility - we're open 7 days a week.</p>
             <span>Copyright 2023. All Rights Reserved</span>
         </div>
-        <div>
-            <div>
-                <a href = "/">Home</a>
-                <a href = "/add">New</a>
+        <div className={styles.footerRight}>
+            <div className={styles.links}>
+                <Link href = "/">Home</Link>
+                <Link href = "/add">New</Link>
             </div>
         </div>
     </footer>
