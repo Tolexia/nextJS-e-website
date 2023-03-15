e07cddec33c64ae8aaafe699c863c8be5afecc91
Axel <axelcroizepro@gmail.com>
2023-03-15 07:43:24 +0100

    fixed apos html entities

--- nextjs-e-website/src/components/footer.js
+++ nextjs-e-website/src/components/footer.js
@@ -7,7 +7,7 @@ export default function Footer()
     <footer className={styles.footer}>
         <div className={styles.footerLeft}>
             <h7>sneakers</h7>
-            <p>Sneakers in an all in one stop to fullfill your style needs. We're a small team of shoes lovers and streetwear specialists who are devoted to helping you shine amongst the crowd.Come and visit our demo facility - we're open 7 days a week.</p>
+            <p>Sneakers in an all in one stop to fullfill your style needs. We are a small team of shoes lovers and streetwear specialists who are devoted to helping you shine amongst the crowd.Come and visit our demo facility - we are open 7 days a week.</p>
             <span>Copyright 2023. All Rights Reserved</span>
         </div>
         <div className={styles.footerRight}>
